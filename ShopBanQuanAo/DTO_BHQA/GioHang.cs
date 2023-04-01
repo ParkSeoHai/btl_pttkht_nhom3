fde1ef71e774ec29f8ba3ffc6101cf52ef471373
ParkSeoHai <anhhai282003@gmail.com>
2023-04-01 19:56:07 +0700

    Them file giohang.cs

---
+++ ShopBanQuanAo/DTO_BHQA/GioHang.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_BHQA
+{
+    public class GioHang
+    {
+        private string _MaKH;
+        private string _MaSP;
+
+        public string MaKH { get => _MaKH; set => _MaKH = value; }
+        public string MaSP { get => _MaSP; set => _MaSP = value; }
+        
+        public GioHang() { }
+        public GioHang(string maKH, string maSP)
+        {
+            _MaKH = maKH;
+            _MaSP = maSP;
+        }
+    }
+}
