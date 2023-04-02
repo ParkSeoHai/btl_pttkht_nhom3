3ba2baa2fdd50575c0c288ffa5bab540cf985785
Your Name <you@example.com>
2023-04-02 22:47:57 +0700

    commit

--- ShopBanQuanAo/GUI_BHQA/Program.cs
+++ ShopBanQuanAo/GUI_BHQA/Program.cs
@@ -16,7 +16,7 @@ namespace GUI_BHQA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmQLSP());
+            Application.Run(new frmQLKH());
         }
     }
 }
