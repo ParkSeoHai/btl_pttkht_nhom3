137f986f4bf0a13d70603e4f899612d0f3b3f5e1
Mr Manh Pc <nguyenxuanmanh2992003@gmail.com>
2023-04-04 17:43:51 +0700

    commit

--- ShopBanQuanAo/DTO_BHQA/DBConnect.cs
+++ ShopBanQuanAo/DTO_BHQA/DBConnect.cs
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
-using System.Security.Policy;
-namespace DTO_BHQA
-{
-    public class DBConnect
-    {
-        // Connect của Hải
-        public static string connStringHai()
-        {
-            string connStringHai = @"Data Source=DELL-VIP-PRO;Initial Catalog=ShopQuanAo;Integrated Security=True";
-            return connStringHai;
-        }
-        public static SqlConnection chuoiKetNoiCua_Hai()
-        {
-            string connStringHai2 = @"Data Source=DELL-VIP-PRO;Initial Catalog=ShopQuanAo;Integrated Security=True";
-            SqlConnection sqlCon = new SqlConnection(connStringHai2);
-            return sqlCon;
-        }
-
-        // Connect của Mạnh
-        public static SqlConnection chuoiKetNoiCua_XuanManh()
-        {
-            string strCon = @"Data Source=DESKTOP-LNJ99RH\SQLEXPRESS;Initial Catalog=ShopQuanAo;Integrated Security=True";
-            SqlConnection sqlCon = new SqlConnection(strCon);
-            sqlCon.Open();
-            return sqlCon;
-        }
-    }
-    // Connect của Duy
-    public class Connection
-    {
-        private static string strConnection = @"Data Source=VANDUY\SQLEXPRESS;Initial Catalog=ShopQuanAo1;Integrated Security=True";
-        public static SqlConnection GetSqlConnection()
-        {
-            return new SqlConnection(strConnection);
-        }
-
-
-    }
-}
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -93,4 +45,3 @@ namespace DTO_BHQA
 
     }
 }
->>>>>>> 165ec7a17958b715d820aaa55a034b0fe6e3b77f
--- ShopBanQuanAo/DTO_BHQA/Modify.cs
+++ ShopBanQuanAo/DTO_BHQA/Modify.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -52,58 +51,3 @@ namespace DTO_BHQA
 
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DTO_BHQA
-{
-    public class Modify
-    {
-
-        public Modify()
-        {
-
-        }
-
-        SqlCommand SqlCommand; // dung de truy van cac cau lenh insert, update,delete...
-        SqlDataReader dataReader; // dung de doc du lieu trong bang
-
-        // tạo một list tài khoản 
-        public List<TaiKhoan> taiKhoans(string query)  // dung check tai khoan
-        {
-            List<TaiKhoan> taiKhoans = new List<TaiKhoan>();
-            // khi using thực thi phần trong ngoăc thì nó sẽ xóa hết đối tượng ở trên 
-            using (SqlConnection sqlConnection = Connection.GetSqlConnection())
-            {
-                sqlConnection.Open();  // mở kết nối 
-                SqlCommand = new SqlCommand(query, sqlConnection); // khởi tạo sqlcommand 
-                dataReader = SqlCommand.ExecuteReader();      // tạo dâtreader để tiến hành đọc dữ liệu 
-                while (dataReader.Read())
-                {            // đọc từng dòng dữ liệu rồi add vào list tài khoản
-                    taiKhoans.Add(new TaiKhoan(dataReader.GetString(0), dataReader.GetString(1), dataReader.GetString(3)));
-                }
-
-                sqlConnection.Close(); // đóng kết nối 
-            }
-            return taiKhoans;   // trả về list tài khoản
-        }
-
-        public void Command(string query)  // dung de dang ki tai khoan
-        {
-            using (SqlConnection sqlConnection = Connection.GetSqlConnection())
-            {
-                sqlConnection.Open();
-                SqlCommand = new SqlCommand(query, sqlConnection);
-                SqlCommand.ExecuteNonQuery(); // thuc thi cau truy van
-                sqlConnection.Close();
-            }
-        }
-
-    }
-}
->>>>>>> 165ec7a17958b715d820aaa55a034b0fe6e3b77f
